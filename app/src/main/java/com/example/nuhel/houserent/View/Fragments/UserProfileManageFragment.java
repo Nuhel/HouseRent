874f174d72d3584a67907532c930bd8fe1f18299
Nuhel <nuheljft@gmail.com>
2017-11-11 23:24:32 +0600

    Multiple Image Upload Added

--- app/src/main/java/com/example/nuhel/houserent/View/Fragments/UserProfileManageFragment.java
+++ app/src/main/java/com/example/nuhel/houserent/View/Fragments/UserProfileManageFragment.java
@@ -2,7 +2,9 @@ package com.example.nuhel.houserent.View.Fragments;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -10,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.nuhel.houserent.Adapter.OwnPostRecyclerViewAdapter;
@@ -19,17 +22,27 @@ import com.example.nuhel.houserent.Controller.GetFirebaseAuthInstance;
 import com.example.nuhel.houserent.Controller.GetFirebaseInstance;
 import com.example.nuhel.houserent.CustomImagePicker.Action;
 import com.example.nuhel.houserent.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import id.zelory.compressor.Compressor;
 
 public class UserProfileManageFragment extends Fragment {
 
 
+    private static StorageReference mStorageRef;
     private RecyclerView recyclerView;
     private View view;
     private RecyclerViewAdapter adapter;
@@ -39,7 +52,10 @@ public class UserProfileManageFragment extends Fragment {
     private DatabaseReference my_postlist_ref;
     private DatabaseReference all_postlist_ref;
     private String userUid;
-
+    private ArrayList<Uri> imagePaths;
+    private int cu = 0;
+    private ImageView imageView;
+    private String postkey;
 
 
     public UserProfileManageFragment() {
@@ -51,6 +67,7 @@ public class UserProfileManageFragment extends Fragment {
 
         userUid = GetFirebaseAuthInstance.getFirebaseAuthInstance().getCurrentUser().getUid();
         all_postlist_ref = GetFirebaseInstance.GetInstance().getReference("HomeAddList");
+        imagePaths = new ArrayList<>();
         initializePostIds();
         view = view == null ? inflater.inflate(R.layout.user_profile_manage, container, false) : view;
         logOutButton = view.findViewById(R.id.logoutbutton);
@@ -62,15 +79,11 @@ public class UserProfileManageFragment extends Fragment {
             }
         });
 
-
-
         postButton = view.findViewById(R.id.postbutton);
-
 
         postButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String postkey = postkeyGenerator();
                 HashMap<String, String> map1 = new HashMap<>();
                 map1.put("area", "nuh");
                 map1.put("image1", "http://www.canmorerealestate.com/assets/images/Featured-Listings/1041-Wilson-Way/Slider/1041-Wilson-Way-Canmore-houce.jpg");
@@ -78,7 +91,6 @@ public class UserProfileManageFragment extends Fragment {
                 my_postlist_ref.child(postkey).setValue("f");
             }
         });
-
 
 
         view.findViewById(R.id.multiImage).setOnClickListener(new View.OnClickListener() {
@@ -89,6 +101,18 @@ public class UserProfileManageFragment extends Fragment {
 
             }
         });
+
+        imageView = view.findViewById(R.id.imageview);
+
+        view.findViewById(R.id.next).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                uploadImages();
+
+            }
+        });
+
 
         recyclerView = view.findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
@@ -103,6 +127,7 @@ public class UserProfileManageFragment extends Fragment {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 postIds.add(dataSnapshot.getKey());
+                postkey = postkeyGenerator();
             }
 
             @Override
@@ -112,9 +137,9 @@ public class UserProfileManageFragment extends Fragment {
 
             @Override
             public void onChildRemoved(DataSnapshot dataSnapshot) {
-
                 int index = postIds.indexOf(dataSnapshot.getKey());
                 postIds.remove(index);
+                postkey = postkeyGenerator();
             }
 
             @Override
@@ -128,7 +153,6 @@ public class UserProfileManageFragment extends Fragment {
             }
         });
     }
-
 
     private String postkeyGenerator() {
         if (postIds.size() > 0) {
@@ -144,15 +168,50 @@ public class UserProfileManageFragment extends Fragment {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        imagePaths.clear();
         if (requestCode == 200 && resultCode == Activity.RESULT_OK) {
-            String[] all_path = data.getStringArrayExtra("all_path");
-
-
-            for (String string : all_path) {
-                Toast.makeText(getContext(), string, Toast.LENGTH_SHORT).show();
+            String[] links = data.getStringArrayExtra("all_path");
+            for (int a = 0; a <= links.length - 1; a++) {
+                File thumb_bitmap = null;
+                try {
+                    thumb_bitmap = new Compressor(getContext())
+                            .setQuality(75)
+                            .setMaxWidth(200)
+                            .setMaxHeight(200)
+                            .compressToFile(new File(links[a]));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                imagePaths.add(Uri.fromFile(thumb_bitmap));
             }
-
         }
+    }
+
+
+    private void uploadImages() {
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        //Storage Reference for main image
+        StorageReference filepath;
+        filepath = mStorageRef.child("post_images").child(postkeyGenerator() + "imgno-" + cu + ".jpeg");
+        filepath.putFile(imagePaths.get(cu)).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                if (task.isSuccessful()) {
+
+                } else {
+
+                }
+                cu++;
+                if (cu <= imagePaths.size() - 1) {
+                    Toast.makeText(getContext(), "Task Done " + String.valueOf(cu), Toast.LENGTH_SHORT).show();
+                    uploadImages();
+                } else {
+                    Toast.makeText(getContext(), "Task Done Uploaded", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
     }
 
 }
--- app/src/main/java/com/example/nuhel/houserent/View/MainActivity.java
+++ app/src/main/java/com/example/nuhel/houserent/View/MainActivity.java
@@ -219,7 +219,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-
+        super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
             if (resultCode == RESULT_OK) {
